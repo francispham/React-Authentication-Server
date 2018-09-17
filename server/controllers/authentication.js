5489534852e4ccfff35952b23c3ad46e6088cbd2
Francis <francispham89@gmail.com>
2018-09-17 13:28:05 -0700

    Create Authentication Controller & User Records

---
+++ server/controllers/authentication.js
@@ -0,0 +1,34 @@
+const User = require('../models/user');
+
+exports.signup = function(req, res, next) {
+  // console.log(req.body);
+  const email = req.body.email;
+  const password = req.body.password;
+  // Steps:
+  // 1. See if a user with given email exists
+  User.findOne({email: email}, function(err, exitingUser) {
+    if (err) { return next(err); }
+
+    // 2. If a user with email does exist, return an error
+    if (exitingUser) {
+      return res.status(422).send({error: 'Email is in use'});
+    }
+
+    // 3. If a user with email does NOT exit, create and save user record
+    const user = new User({
+      email: email,
+      password: password
+    });
+
+    user.save(function(err) {
+      if (err) { return next(err); }
+
+      // Respond to request indicating the user was created
+      res.json({ success: true });
+    });
+  })
+/*
+Sample:
+  res.send({success: 'true'});
+*/
+}
--- server/router.js
+++ server/router.js
@@ -1,6 +1,10 @@
+const Authentication = require('./controllers/authentication');
+
 module.exports = function(app) {
+/* Get Request Sample:
   app.get('/', function(req, res, next) {
     res.send(["waterbottle", "phone", "paper"])
   });
-
+*/
+  app.post('/signup', Authentication.signup);
 }
